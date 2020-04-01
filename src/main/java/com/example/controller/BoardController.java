d13836a7de7b04c0a83944c7c3dc99e3287baf1d
naoto-wataya <naoto.wataya@rakus.co.jp>
2020-04-01 14:43:33 +0900

    create index() method in BoardController class

--- src/main/java/com/example/controller/BoardController.java
+++ src/main/java/com/example/controller/BoardController.java
@@ -1,5 +1,30 @@
 package com.example.controller;
 
-public class BoardController {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.Post;
+
+@Controller
+@RequestMapping("/application-scope")
+public class BoardController {
+	
+	@Autowired
+	private ServletContext application;
+	
+	@RequestMapping("")
+	public String index() {
+		List<Post> posts = (List<Post>)application.getAttribute("posts");
+		if(posts == null) {
+			posts = new ArrayList<Post>();
+		}
+		application.setAttribute("posts", posts);
+		return "msg-board";
+	}
 }
