c1ed195156916231a2311802c89f6a7b4212d186
naoto-wataya <naoto.wataya@rakus.co.jp>
2020-04-01 14:24:15 +0900

    add Post class to form package.

---
+++ src/main/java/com/example/form/Post.java
@@ -0,0 +1,20 @@
+package com.example.form;
+
+public class Post {
+	private String name;
+	private String comment;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+}
